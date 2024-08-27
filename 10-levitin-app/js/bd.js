776c5cc17d9ebfaaf8caad693fcfaf8e139ec166
IsabellaBarrero <isabella.barrero.93@gmail.com>
2024-08-27 16:06:17 -0500

    avance

---
+++ 10-levitin-app/js/bd.js
@@ -0,0 +1,87 @@
+let modulos = [
+    {
+        id: 1,
+        titulo: 'Módulo 1',
+        subtitulo: 'Introducción a la psicología musical',
+        descripcion: 'Abarcaremos tanto la historia y evolución, como la importancia y prácticas de la psicología musical',
+        temas: [
+            {
+                titulo: 'Introducción a la psicología musical',
+                descripcion: 'A lo largo de la historia la música ha acompañado al ser humano, desde tiempos inmemorables los sonidos transmiten emociones y han sido una forma de expresión humana, lingüística y cultural. Durante años la música ha sido objeto de estudio, la humanidad ha explorado su relación con la mente y es ahí cuando aparece la psicología, la ciencia que estudia los procesos mentales.',
+                video: '',
+            },
+
+            {
+                titulo: '¿Qué es la psicología musical?',
+                descripcion: 'La psicología musical es un campo interdisciplinario de la psicología que estudia cómo las personas perciben, comprenden, experimentan y se relacionan con la música, así como los efectos de la música en la mente, el cuerpo y el comportamiento humano.',
+                video: '',
+                caja:'',
+            },
+
+            {
+                titulo: 'Historia y evolución de la psicología musical',
+                descripcion: 'En la antigüedad, se creía frmemente en el poder de la música para infuir en la salud física, mental y espiritual de las personas. En muchas cultras antiguas como la egipcia, la griega, la china e incluso la mesopotámica, se utilizaba la música como una herramienta terapéutica para promover la curación y el bienestar.',
+                video: '',
+                caja: '',
+            },
+
+            {
+                titulo: 'Importacia y aplicaciones prácticas de la psicología musical',
+                descripcion: 'La psicología musical ayuda a comprender cómo la música afecta la mente y el comportamiento humano, y cómo podemos utilizar este conocimiento de manera práctica en diversas áreas, desde la salud y el bienestar hasta la educación y el entretenimiento.',
+                video: '',
+                formlario: '',
+               
+            },
+        ]
+    },
+
+    {
+        id: 2,
+        titulo: 'Módulo 2',
+        subtitulo: 'Percepción musical',
+        descripcion: 'Abarcaremos desde los elementos de la música (tono, ritmo, melodía y armonía) hasta el procesamiento auditivo',
+        temas: [
+            {
+                titulo: 'Percepción musical',
+                descripcion: 'La percepción musical es la capacidad de interpretar y comprender la música a través de los sentidos, especialmente el oído. Esta área también implica la capacidad de reconocer patrones musicales, identifcar instrumentos y comprender el signifcado emocional y expresivo de la música. ',
+                video: '',
+            },
+
+            {
+                titulo: 'Elementos de la música: tono, ritmo, melodía y armonía',
+                cards: [
+                    {
+                        titulo: 'Tono',
+                        descripcion: 'En música el término tono tiene dos significados: 1 Altura: Se refere a lo agudo o grave que percibimos un sonido. La altura de un sonido está determinada por su frecuencia, que es la cantidad de vibraciones que produce por segundo. Cuanto mayor sea la frecuencia más agudo es el sonido, en cambio cuanto menor sea la frecuencia, más grave será el sonido. 2 Intervalo: Es la distancia entre dos notas, ya sea en términos de altura (frecuencia) o en términos de posición en una escala musical. este concepto es esencial para comprender cómo se construyen las melodías, armonías y escalas.',
+                    },
+
+                    {
+                        titulo: 'Ritmo',
+                        descripcion: 'En música, el ritmo es la organización de los sonidos en el tiempo. Es lo que le da a la música su pulso y su movimiento. La duración de los sonidos, los acentos y los silencios son los que ayudan a crear el ritmo de una canción, por ejemplo, la música pop suele tener un ritmo simple y regular, con un tempo rápido y acentos fuertes. Todos los tipos de ritmo pueden crear una gran variedad de emociones y sensaciones en el oyente.',
+                    },
+                ],
+                video: '',
+                caja:'',
+            },
+
+            {
+                titulo: 'Historia y evolución de la psicología musical',
+                descripcion: 'En la antigüedad, se creía frmemente en el poder de la música para infuir en la salud física, mental y espiritual de las personas. En muchas cultras antiguas como la egipcia, la griega, la china e incluso la mesopotámica, se utilizaba la música como una herramienta terapéutica para promover la curación y el bienestar.',
+                video: '',
+                caja: '',
+            },
+
+            {
+                titulo: 'Importacia y aplicaciones prácticas de la psicología musical',
+                descripcion: 'La psicología musical ayuda a comprender cómo la música afecta la mente y el comportamiento humano, y cómo podemos utilizar este conocimiento de manera práctica en diversas áreas, desde la salud y el bienestar hasta la educación y el entretenimiento.',
+                video: '',
+                formlario: '',
+               
+            },
+        ]
+    }
+
+
+
+
+]
