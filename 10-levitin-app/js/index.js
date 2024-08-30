87f4efd9e9f28038e81ccfb90750c72909eb9221
IsabellaBarrero <isabella.barrero.93@gmail.com>
2024-08-30 15:41:07 -0500

    avance2

--- 10-levitin-app/js/index.js
+++ 10-levitin-app/js/index.js
@@ -12,3 +12,36 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+const saludo = document.querySelector('.saludo');
+
+let nombreUsuario = JSON.parse(localStorage.getItem('user'));
+
+saludo.innerHTML = `Hola ${nombreUsuario.userName} 🎶🎶`
+
+
+const btnIniciarSesion = document.querySelector('.inicio-sesion');
+const btnRegistro = document.querySelector('.registro');
+const saludoUsuario = document.querySelector('.saludo');
+
+
+
+// Verifica si el usuario está registrado 
+function verificarSesion() {
+    let currentUser = JSON.parse(localStorage.getItem('user'));
+
+    if (currentUser) {
+        // Si hay un usuario logueado, mostrar su nombre y ocultar botones
+        saludoUsuario.textContent = `Bienvenido, ${currentUser.userName} 🎶🎶`;
+        btnIniciarSesion.style.display = 'none';
+        btnRegistro.style.display = 'none';
+    } else {
+        // Si no hay usuario logueado, mostrar los botones y ocultar el nombre
+        saludoUsuario.textContent = '';
+        btnIniciarSesion.style.display = 'inline-block';
+        btnRegistro.style.display = 'inline-block';
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', verificarSesion);
---
+++ 10-levitin-app/js/registro.js
@@ -0,0 +1,25 @@
+const formulario = document.querySelector('#formulario');
+const nombreUsuario = document.querySelector('#nombreUsuario');
+const email = document.querySelector('#emailUsuario');
+const contraseña = document.querySelector('#contrasenaUsuario');
+
+
+function registrarUser(e){
+    e.preventDefault();
+
+
+    //Registrar el dato de "user" en localStorage
+    let user = {
+    userName: nombreUsuario.value.toLowerCase(),
+    userPass: contraseña.value.toLowerCase(),
+    userEmail: email.value.toLowerCase(),
+
+}
+
+    localStorage.setItem('user', JSON.stringify(user))
+
+    formulario.reset();
+}
+
+
+formulario.addEventListener('submit', registrarUser)
